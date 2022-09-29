4b2e8674d8945d21b5ae3e3558630827b25fe6b4
MaxGrondin <114398130+MaxGrondin@users.noreply.github.com>
2022-09-29 08:24:09 +0200

    Add files via upload

---
+++ plateforme_1 création personnage/Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+###### FAIRE BOUGER DANS 4 DIRECTIONS ######
+
+var speed = 250
+var velocity = Vector2()
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('ui_down'):
+		velocity.y += 1
+	if Input.is_action_pressed('ui_up'):
+		velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
